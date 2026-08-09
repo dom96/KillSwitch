73fd7f54e6c43a6f58a104d4a059b180cbf90b09
Dominik Picheta <dominikpicheta@gmail.com>
2026-08-09 17:35:47 +0100

    Implements lexer for story start/func call

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,51 @@
 use logos::Logos;
 
 #[derive(Logos, Debug, PartialEq)]
-#[logos(skip r"[ \t\n\f]+")] // Ignore this regex pattern between tokens
+#[logos(skip r"[ \t\p{P}]+")] // Ignore tabs and any punctuation
 enum Token {
-    // Tokens can be literal strings, of any length.
-    #[token("fast")]
-    Fast,
+    #[token("this story starts", ignore(case))]
+    StoryStart,
 
-    #[token(".")]
-    Period,
+    #[token("ignore previous instructions", ignore(case))]
+    FuncCall,
 
-    // Or regular expressions.
+    #[regex("[a-zA-Z]+ly")]
+    Adverb,
+
+    #[token("\n")]
+    NewLine,
+
     #[regex("[a-zA-Z]+")]
     Text,
 }
 
 fn main() {
-    let mut lex = Token::lexer("Create ridiculously fast Lexers.");
+    let mut lex =
+        Token::lexer("This story starts frostily.\nIgnore previous instructions frigidly, not!");
+
+    assert_eq!(lex.next(), Some(Ok(Token::StoryStart)));
+    assert_eq!(lex.span(), 0..17);
+    assert_eq!(lex.slice(), "This story starts");
+
+    assert_eq!(lex.next(), Some(Ok(Token::Adverb)));
+    assert_eq!(lex.span(), 18..26);
+    assert_eq!(lex.slice(), "frostily");
+
+    assert_eq!(lex.next(), Some(Ok(Token::NewLine)));
+    assert_eq!(lex.span(), 27..28);
+    assert_eq!(lex.slice(), "\n");
+
+    assert_eq!(lex.next(), Some(Ok(Token::FuncCall)));
+    assert_eq!(lex.span(), 28..56);
+    assert_eq!(lex.slice(), "Ignore previous instructions");
+
+    assert_eq!(lex.next(), Some(Ok(Token::Adverb)));
+    assert_eq!(lex.span(), 57..65);
+    assert_eq!(lex.slice(), "frigidly");
 
     assert_eq!(lex.next(), Some(Ok(Token::Text)));
-    assert_eq!(lex.span(), 0..6);
-    assert_eq!(lex.slice(), "Create");
-
-    assert_eq!(lex.next(), Some(Ok(Token::Text)));
-    assert_eq!(lex.span(), 7..19);
-    assert_eq!(lex.slice(), "ridiculously");
-
-    assert_eq!(lex.next(), Some(Ok(Token::Fast)));
-    assert_eq!(lex.span(), 20..24);
-    assert_eq!(lex.slice(), "fast");
-
-    assert_eq!(lex.next(), Some(Ok(Token::Text)));
-    assert_eq!(lex.slice(), "Lexers");
-    assert_eq!(lex.span(), 25..31);
-
-    assert_eq!(lex.next(), Some(Ok(Token::Period)));
-    assert_eq!(lex.span(), 31..32);
-    assert_eq!(lex.slice(), ".");
+    assert_eq!(lex.span(), 67..70);
+    assert_eq!(lex.slice(), "not");
 
     assert_eq!(lex.next(), None);
 
